a6d08434394b689a09edd9a886e901b3b8619ce6
Divi Newton <divinewton@gmail.com>
2025-06-19 22:20:27 -0700

    Initial chat ui changes

--- components/ui/enter-task-card.tsx
+++ components/ui/enter-task-card.tsx
@@ -16,29 +16,31 @@ export default function EnterTaskCard({
     <div className="flex items-start justify-center pt-10 px-5 md:px-10 enter-task-sticky max-w-full">
       <Card className="w-full max-w-2xl transition-transform duration-0 hover:scale-100">
         <CardContent>
-          <div className="flex flex-row gap-3 items-center justify-center w-full">
-            <Textarea
-              placeholder="Generate a checklist for any task..."
-              id="prompt"
-              value={input}
-              onChange={e => setInput(e.target.value)}
-              className="self-end flex-1 w-full"
-              onKeyDown={e => {
-                if (e.key === "Enter" && !e.shiftKey) {
-                  e.preventDefault();
-                  onSubmit();
-                }
-              }}
-            />
-            <Button
-              type="button"
-              variant="default"
-              className="self-end"
-              onClick={onSubmit}
-              size="icon"
-            >
-              <SendHorizontal className="h-[2rem] w-[2rem] scale-100 " />
-          </Button>
+          <div className="w-full flex justify-center items-center">
+            <div className="relative w-full">
+              <Textarea
+                placeholder="Generate a checklist for any task..."
+                id="prompt"
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                className="flex-1 w-full rounded-xl pr-12"
+                onKeyDown={e => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    onSubmit();
+                  }
+                }}
+              />
+              <Button
+                type="button"
+                variant="default"
+                className="absolute bottom-2 right-2"
+                onClick={onSubmit}
+                size="icon"
+              >
+                <SendHorizontal className="h-[2rem] w-[2rem] scale-100 " />
+              </Button>
+            </div>
           </div>
         </CardContent>
       </Card>
